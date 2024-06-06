76d762d88a86315a8d031ecfa7c4c5f43929adfd
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-06-07 01:38:34 +0300

    add_localOptional_for_multi_file_add

--- src/category/storageFilesCategories.ts
+++ src/category/storageFilesCategories.ts
@@ -1,11 +1,25 @@
-import {diskStorage} from "multer";
-import {normalSizeFileName} from "../utils/forFiles";
+import { diskStorage } from 'multer';
+import { normalSizeFileName } from '../utils/forFiles';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
+export const pathUploadCategory = './uploads/categories';
+export const fileStorage = diskStorage({
+  destination: pathUploadCategory,
+  filename: normalSizeFileName,
+});
 
-export const pathUploadCategory = './uploads/categories'
-export const fileStorage = diskStorage(
-    {
-        destination: pathUploadCategory,
-        filename: normalSizeFileName
-    }
-)
+export const localOptions = {
+  storage: fileStorage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter(req: any, file: any, callback: any) {
+    if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) callback(null, true);
+    else
+      callback(
+        new HttpException(
+          `Unsupported file type ${file.originalname}`,
+          HttpStatus.BAD_REQUEST,
+        ),
+        false,
+      );
+  },
+};
--- src/product/storageFilesProducts.ts
+++ src/product/storageFilesProducts.ts
@@ -1,10 +1,25 @@
-import {diskStorage} from "multer";
-import {normalSizeFileName} from "../utils/forFiles";
+import { diskStorage } from 'multer';
+import { normalSizeFileName } from '../utils/forFiles';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
-export const pathUploadProduct = './uploads/products'
-export const fileStorage = diskStorage(
-    {
-        destination: pathUploadProduct,
-        filename: normalSizeFileName,
-    }
-)
+export const pathUploadProduct = './uploads/products';
+export const fileStorage = diskStorage({
+  destination: pathUploadProduct,
+  filename: normalSizeFileName,
+});
+
+export const localOptions = {
+  storage: fileStorage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter(req: any, file: any, callback: any) {
+    if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) callback(null, true);
+    else
+      callback(
+        new HttpException(
+          `Unsupported file type ${file.originalname}`,
+          HttpStatus.BAD_REQUEST,
+        ),
+        false,
+      );
+  },
+};
